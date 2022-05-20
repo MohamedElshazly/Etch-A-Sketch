d32e982c0ba73866e50ccabbb5a0edc360c5fc5c
omni <mohamedelshazly18@yahoo.com>
2022-05-20 23:40:53 +0200

    added event listner to draw on canvas

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const grid = document.querySelector('.grid');
 //create div(grid squares)
 for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('square');
-    gridSquare.setAttribute('style', 'width: 60; height:60;');
-    // gridSquare.innerText = `gridSquare ${i+1} `; 
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('square');
+        gridSquare.setAttribute('style', 'width: 60; height:60;');
+        // gridSquare.innerText = `gridSquare ${i+1} `; 
     
-    //add to grid
-    grid.appendChild(gridSquare);
-}
-}
+        //add to grid
+        grid.appendChild(gridSquare);
+    };
+};
+
+// get 
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mousemove', (e) => {
+        if(e.buttons == 1 ){
+            square.setAttribute('style', 'background-color:black;');
+        }
+})
+
+})
